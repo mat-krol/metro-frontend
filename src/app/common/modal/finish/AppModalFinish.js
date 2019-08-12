d07b7d6af4d4439ab5c519c6ad3291b2f3b602f4
unknown <mateusz.damian.krol@gmail.com>
2019-08-12 15:36:06 +0200

    create a new finalising saga

--- src/app/common/modal/finish/AppModalFinish.js
+++ src/app/common/modal/finish/AppModalFinish.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import * as selectors from '../../../../redux/selectors'
-// import { continueModeExpand } from '../../../../redux/sagas/mode'
+import { finishGameRound } from '../../../../redux/sagas/round'
 import { ROUTES } from '../../../constants/routes';
 
 import Modal from '../../../../ui/modal/Modal';
 import Button from '../../../../ui/button/Button';
 
 function AppModalFinish({ show }) {
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const satisfaction = useSelector(selectors.getRoundSatisfaction)
+
+  const finishGame = () => {
+    dispatch(finishGameRound.trigger())
+  }
   
   return (
-    <Modal show={satisfaction > 9}>
+    <Modal show={satisfaction > 5}>
       <h1>Round finished!</h1>
-      <p style={{ marginTop: "0" }}>Well done, you've achieved 5.0 satisfaction in all neighbourhoods</p>
-      <Button to={ROUTES.MenuHome} text="OK" />
+      <p style={{ marginTop: "0" }}>Well done, you've achieved an average satisfaction of 9.0 across all neighbourhoods</p>
+      <Button to={ROUTES.MenuHome} text="OK" onClick={finishGame} />
     </Modal>
   )
 }
