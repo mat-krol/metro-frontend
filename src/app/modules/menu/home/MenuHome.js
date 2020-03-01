34053c7160a3c35de1762ba5c54c706577c37079
unknown <mateusz.damian.krol@gmail.com>
2020-03-01 21:48:46 +0000

    small AppModule refactor

--- src/app/modules/menu/home/MenuHome.js
+++ src/app/modules/menu/home/MenuHome.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { ROUTES } from '../../../constants/routes';
+import React from "react";
+import { ROUTES } from "../../../constants/routes";
 // import { useDispatch } from 'react-redux'
 // import { getLevels } from '../../../../redux/sagas/game'
 
-import logo from '../../../../assets/icon-white.png'
-import AppModule from '../../../common/module';
-import Selection from '../../../../ui/selection/Selection';
+import logo from "../../../../assets/icon-white.png";
+import AppModule from "../../../common/module";
+import Selection from "../../../../ui/selection/Selection";
 
 function MenuHome() {
   // const dispatch = useDispatch()
 
-  // React.useEffect(() => {                   
+  // React.useEffect(() => {
   //   dispatch(getLevels.trigger())
   // }, [dispatch])
 
   return (
     <AppModule type="select" header>
-      <img src={logo} alt="logo" style={{ width: "115px", marginBottom: "2rem"}} />
+      <img
+        src={logo}
+        alt="logo"
+        style={{ width: "115px", marginBottom: "2rem" }}
+      />
       <Selection list={list} />
     </AppModule>
-  )
+  );
 }
 
 const list = [
   { to: ROUTES.GameHome, text: "Demo" },
+  { to: ROUTES.MenuBoard, text: "Demo" },
+  { to: ROUTES.MenuCountry, text: "Demo" },
   { to: ROUTES.Settings, text: "Settings", disabled: true },
   { to: ROUTES.GameHome, text: "Log out", disabled: true }
-]
+];
 
 export default MenuHome;
